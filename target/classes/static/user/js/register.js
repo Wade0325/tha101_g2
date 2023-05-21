20fdb577d30658df872009f3c05bd6446d8d2d3e
Wade <wl02014258@gmail.com>
2023-05-21 16:49:43 +0800

    新增: 網站基本架構

---
+++ target/classes/static/user/js/register.js
@@ -0,0 +1,64 @@
+(() => {
+	const btn1 = document.querySelector('#btn');
+	const msg = document.querySelector('#msg');
+	const email = document.querySelector('#email');
+	const password = document.querySelector('#password');
+	const checkPassword = document.querySelector('#checkPassword');
+	const phone = document.querySelector('#phone');
+	const address = document.querySelector('#address');
+	const inputs = document.querySelectorAll('input');
+	btn.addEventListener('click', () => {
+		const accLength = email.value.length;
+	console.log(accLength);
+		if (accLength < 5 || accLength > 50) {
+			msg.textContent = '帳號長度須介於5~50字元';
+			return;
+		}
+
+		const pwdLength = password.value.length;
+		if (pwdLength < 6 || pwdLength > 12) {
+			msg.textContent = '密碼長度須介於6~12字元';
+			return;
+		}
+
+		if (checkPassword.value !== password.value) {
+			msg.textContent = '密碼與確認密碼不相符';
+			return;
+		}
+
+		const phoneLength = phone.value.length;
+		if (phoneLength != 10) {
+			msg.textContent = '電話號碼長度錯誤';
+			return;
+		}
+
+		msg.textContent = '';
+		fetch('http://localhost:8080/THA101_G2/user/register2', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: email.value,
+				password: password.value,
+				nickname: phone.value,
+			}),
+		})
+			.then(resp => resp.json())
+			.then(body => {
+				const { successful } = body;
+				if (successful) {
+					for (let input of inputs) {
+						input.disabled = true;
+					}
+					btn1.disabled = true;
+					msg.className = 'info';
+					msg.textContent = '註冊成功';
+				} else {
+					msg.className = 'error';
+					msg.textContent = '註冊失敗';
+				}
+			});
+	});
+
+})();
