e491503c79957a8b84f2f0205b363ebde26830b8
Wade <wl02014258@gmail.com>
2023-05-22 11:37:25 +0800

    新增: 寄信功能post成功

---
+++ src/main/java/tw/idv/petpet/web/user/entity/EmailDetails.java
@@ -0,0 +1,22 @@
+// Java Program to Illustrate EmailDetails Class
+package tw.idv.petpet.web.user.entity;
+
+// Importing required classes
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Annotations
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+// Class
+public class EmailDetails {
+
+	// Class data members
+	private String recipient;
+	private String msgBody;
+	private String subject;
+	private String attachment;
+}
