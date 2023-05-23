3ea5dc1af722e5fe0f7a63c86f88c50922bee2ac
Wade <wl02014258@gmail.com>
2023-05-23 17:00:25 +0800

    新增: 會員中心網頁

---
+++ src/main/resources/static/user/js/member.center.js
@@ -0,0 +1,26 @@
+$("#btn_update_nickName").on("click", function () {
+    console.log(nickName.value)
+    fetch('insert', () => {
+
+    })
+
+    window.alert("修改成功")
+})
+
+$("#btn_update_phone").on("click", function () {
+    console.log(phone.value)
+    fetch('insert', () => {
+
+    })
+
+    window.alert("修改成功")
+})
+
+$("#btn_update_address").on("click", function () {
+    console.log(address.value)
+    fetch('insert', () => {
+
+    })
+
+    window.alert("修改成功")
+})
