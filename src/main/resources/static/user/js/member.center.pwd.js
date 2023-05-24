9506a763672d406fdc26a53bd7274d856d0e1eee
Wade <wl02014258@gmail.com>
2023-05-24 14:40:23 +0800

    新增: 會員購買紀錄網頁

--- src/main/resources/static/user/js/member.center.pwd.js
+++ src/main/resources/static/user/js/member.center.pwd.js
@@ -1,11 +1,35 @@
-$("#btn_update_address").on("click", function () {
-    if (window.confirm("確定要修改密碼?")) {
-        if (newPassword.value == confirmPassword.value) {
-            console.log(newPassword.value)
-            fetch('insert', () => {
-
-            })
+(() => {
+    const msg = document.querySelector("#msg")
+    $("#btn_update_address").on("click", function () {
+        if (oldPassword.value == '') {
+            msg.textContent = '舊密碼不得為空';
+            return;
         }
-    }
 
-})
+        if (newPassword.value == '') {
+            msg.textContent = '新密碼不得為空';
+            return;
+        }
+
+        if (confirmPassword.value == '') {
+            msg.textContent = '確認新密碼不得為空';
+            return;
+        }
+
+
+        if (window.confirm("確定要修改密碼?")) {
+            msg.textContent = '';
+            if (newPassword.value == confirmPassword.value) {
+                console.log(newPassword.value)
+                fetch('insert', () => {
+
+                })
+            } else {
+                msg.textContent = '';
+                window.confirm("新密碼與確認密碼不相符")
+            }
+        }
+
+    })
+
+})()
