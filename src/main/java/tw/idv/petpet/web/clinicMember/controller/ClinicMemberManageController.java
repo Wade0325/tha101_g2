a3aeb4fe5744477e3480341acbf9cc5db0b8d104
Paul <paulyang1003@gmail.com>
2023-05-25 22:05:51 +0800

    no message

--- src/main/java/tw/idv/petpet/web/clinicMember/controller/ClinicMemberManageController.java
+++ src/main/java/tw/idv/petpet/web/clinicMember/controller/ClinicMemberManageController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
