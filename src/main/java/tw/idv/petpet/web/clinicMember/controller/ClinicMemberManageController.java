8c195295020e38f6a7d07d31188e745b9bcf2ccd
Paul <paulyang1003@gmail.com>
2023-05-25 21:30:00 +0800

    新增clinicMember findAll前端顯示

--- src/main/java/tw/idv/petpet/web/clinicMember/controller/ClinicMemberManageController.java
+++ src/main/java/tw/idv/petpet/web/clinicMember/controller/ClinicMemberManageController.java
@@ -3,10 +3,13 @@ package tw.idv.petpet.web.clinicMember.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import tw.idv.petpet.web.clinicMember.entity.ClinicMember;
 import tw.idv.petpet.web.clinicMember.service.ClinicMemberService;
@@ -18,10 +21,17 @@ public class ClinicMemberManageController {
 	@Autowired
 	private ClinicMemberService service;
 
-	@GetMapping
-	public String manage(Model model) {
-		List<ClinicMember> clinicMemberList = service.findAll();
-		model.addAttribute("clinicMemberList", clinicMemberList);
-		return "../WEB-INF/clinicMember/manage.jsp";
+//	@GetMapping
+//	public String manage(Model model) {
+//		List<ClinicMember> clinicMemberList = service.findAll();
+//		model.addAttribute("clinicMemberList", clinicMemberList);
+//		return "../WEB-INF/clinicMember/manage.jsp";
+//	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public List<ClinicMember> manage() {
+	    List<ClinicMember> clinicMemberList = service.findAll();
+	    return clinicMemberList;
 	}
 }
