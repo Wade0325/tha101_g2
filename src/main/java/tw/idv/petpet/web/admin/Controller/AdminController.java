26a91479bf34d98f5b9cbfae17842b356ef70a91
Che <cat860816@gmail.com>
2023-05-25 15:13:40 +0800

    no message

--- src/main/java/tw/idv/petpet/web/admin/Controller/AdminController.java
+++ src/main/java/tw/idv/petpet/web/admin/Controller/AdminController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import tw.idv.petpet.web.admin.Repository.AdminRepository;
 import tw.idv.petpet.web.admin.entity.Admin;
 
 @Controller
+//@RequestMapping("/AdminController")
 public class AdminController {
 	@Autowired
 	private AdminRepository adminRepository;
@@ -18,6 +20,10 @@ public class AdminController {
 	@GetMapping("/getAdmin")
 	public String getAdmin() {
 
-		return "admin/adminLogin";
+		return "/admin/adminLogin.html";
+	}
+	@GetMapping("/getall")
+	public List<Admin> getAll() {
+		return adminRepository.findAll();
 	}
 }
