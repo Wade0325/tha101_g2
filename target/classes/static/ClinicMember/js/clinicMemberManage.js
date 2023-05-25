dcc8d22a7ceeec0a9accc2993302d9cc683c3992
Wade <wl02014258@gmail.com>
2023-05-25 08:50:34 +0800

    no message

--- target/classes/static/ClinicMember/js/clinicMemberManage.js
+++
@@ -1,8 +0,0 @@
-(() => {
-	const btn = document.getElementById('btn-manage')
-
-	btn.addEventListener('click', function() {
-		console.log("有觸發")
-		window.location.href = 'manage.html';
-	})
-})();
