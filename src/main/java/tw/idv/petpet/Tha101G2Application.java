09773ff8f4e91c5950b7308336ff119d16c0b90c
Jude <bb77365@gmail.com>
2023-05-29 15:30:54 +0800

    test

--- src/main/java/tw/idv/petpet/Tha101G2Application.java
+++ src/main/java/tw/idv/petpet/Tha101G2Application.java
@@ -2,12 +2,15 @@ package tw.idv.petpet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Tha101G2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tha101G2Application.class, args);
+		
+		
 	}
 
 }
