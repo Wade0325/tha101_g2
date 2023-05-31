b84a98f4a823fa1e07718f20a904ac2aec9f1303
Yachu <yachut12@gmail.com>
2023-05-31 21:26:36 +0800

    no message

--- src/main/java/tw/idv/petpet/web/clinicMember/controller/PriceListController.java
+++ src/main/java/tw/idv/petpet/web/clinicMember/controller/PriceListController.java
@@ -1,15 +1,20 @@
 package tw.idv.petpet.web.clinicMember.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import tw.idv.petpet.web.clinicMember.Repository.PriceListRespository;
+import tw.idv.petpet.web.clinicMember.entity.ClinicMember;
 import tw.idv.petpet.web.clinicMember.entity.PriceList;
 
 @RestController
@@ -18,6 +23,7 @@ public class PriceListController {
 	
 	@Autowired
 	private PriceListRespository priceListRespository;
+	private List<PriceList> PriceList;
 	
 //	@PostMapping
 //	public String insert(@RequestBody PriceList priceList) {
@@ -59,4 +65,13 @@ public class PriceListController {
 		PriceList priceList = priceListRespository.findById(clinicServiceId).orElse(null);
 		 return priceList;
 	}
+	
+//	//findAll
+//	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//	@ResponseBody
+//	public List<PriceList> manage() {
+//		List<PriceList> priceList = priceListRespository.findAll();
+//		return priceList;
+//	}
+
 }
