aa1f9004f34f74808b8b818e879c42ed850d7aeb
Jude <bb77365@gmail.com>
2023-06-02 20:37:16 +0800

    no message

--- src/main/resources/static/clinicMember/js/clinicMemberManage.js
+++ src/main/resources/static/clinicMember/js/clinicMemberManage.js
@@ -1,54 +1,8 @@
-function deleteClick(clinicId) {
-	if (!confirm('是否刪除?')) {
-		return;
-	}
-	fetch(`manage/${clinicId}`, {
-		method: `DELETE`,
+(() => {
+	const btn = document.getElementById('btn-manage')
+
+	btn.addEventListener('click', function() {
+		console.log("有觸發")
+		window.location.href = 'manage.html';
 	})
-		.then(resp => resp.json())
-		.then(body => {
-			if (body.successful) {
-				location.reload();
-			}
-		});
-}
-
-function saveClick(clinicId) {
-	const clinicName = document.querySelector(`#clinicname${clinicId}`).textContent;
-	const clinicPrincipal = document.querySelector(`#clinicprincipal${clinicId}`).textContent;
-	const clinicPassword = document.querySelector(`#clinicpassword${clinicId}`).textContent;
-	const clinicPhone = document.querySelector(`#clinicphone${clinicId}`).textContent;
-	const clinicAddress = document.querySelector(`#clinicaddress${clinicId}`).textContent;
-	const clinicService = document.querySelector(`#clinicservice${clinicId}`).textContent;
-	// const clinicEmail = sessionStorage.getItem('clinicemail');
-	const clinicEmail = document.querySelector(`#clinicemail${clinicId}`).textContent;
-	console.log(clinicEmail)
-	fetch('manage', {
-		method: 'PUT',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({
-			clinicName,
-			clinicPrincipal,
-			clinicPassword,
-			clinicPhone,
-			clinicAddress,
-			clinicService,
-			clinicEmail
-		})
-	})
-		.then(resp => resp.json())
-		.then(body => {
-			const { successful } = body;
-			if (successful) {
-				alert('修改成功!');
-				location.reload();
-			} else {
-				alert('修改失敗');
-				location.reload();
-			}
-		});
-}
-
-
-
-
+})();
--- src/main/resources/static/clinicMember/js/clinicMemberRegister.js
+++ src/main/resources/static/clinicMember/js/clinicMemberRegister.js
@@ -1,13 +1,4 @@
 (() => {
-	const btn = document.getElementById('btn-manage')
-
-	btn.addEventListener('click', function() {
-		console.log("有觸發")
-		window.location.href = 'manage.html';
-	})
-})();
-
-(() => {
 	const btn = document.querySelector('#btn');
 	const msg = document.querySelector('#msg');
 	const clinicName = document.querySelector('#clinicName');
@@ -18,9 +9,7 @@
 	const clinicPassword = document.querySelector('#clinicPassword');
 	const checkPassword = document.querySelector('#checkPassword');
 	const clinicService = document.querySelector('#clinicService');
-	const clinicPhoto = document.querySelector('#clinicPhoto');
 	const inputs = document.querySelectorAll('input');
-
 
 	btn.addEventListener('click', () => {
 
@@ -73,56 +62,39 @@
 		}
 
 		msg.textContent = '';
-		const fileReader = new FileReader();
-		fileReader.addEventListener('load', e => {
-			const clinicPhotoBase64 = btoa(e.target.result);
-			fetch('register', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					clinicName: clinicName.value,
-					clinicPrincipal: clinicPrincipal.value,
-					clinicEmail: clinicEmail.value,
-					clinicPassword: clinicPassword.value,
-					clinicPhone: clinicPhone.value,
-					clinicAddress: clinicAddress.value,
-					clinicService: clinicService.value,
-					clinicPhoto: clinicPhotoBase64
-				}),
-			})
-				.then(resp => resp.json())
-				.then(body => {
-					//解構賦值 const successful = body.successful
-					const { successful, message } = body;
-					if (successful) {
-						for (let input of inputs) {
-							input.disabled = true;
-						}
-						btn.disabled = true;
-						msg.className = 'info';
-						msg.textContent = '註冊成功';
-					} else {
-						msg.className = 'error';
-						msg.textContent = '註冊失敗，' + message;
+		fetch('register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				clinicName: clinicName.value,
+				clinicPrincipal: clinicPrincipal.value,
+				clinicEmail: clinicEmail.value,
+				clinicPassword: clinicPassword.value,
+				clinicPhone: clinicPhone.value,
+				clinicAddress: clinicAddress.value,
+				clinicService: clinicService.value
 
+			}),
+		})
+			.then(resp => resp.json())
+			.then(body => {
+				//解構賦值 const successful = body.successful
+				const { successful, message } = body;
+				if (successful) {
+					for (let input of inputs) {
+						input.disabled = true;
 					}
-				});
-		});
-		fileReader.readAsArrayBuffer(clinicPhoto.files[0]);
-	});
-	const img = document.querySelector('#img');
-	clinicPhoto.addEventListener('change', () => {
-		const file = clinicPhoto.files[0];
-		if (file) {
-			const fileReader = new FileReader();
-			fileReader.onload = function(e) {
-				img.src = e.target.result;
-				console.log(img.src);
-			};
-			fileReader.readAsDataURL(file);
-		}
+					btn.disabled = true;
+					msg.className = 'info';
+					msg.textContent = '註冊成功';
+				} else {
+					msg.className = 'error';
+					msg.textContent = '註冊失敗，' + message;
+
+				}
+			});
 	});
 
 })();
