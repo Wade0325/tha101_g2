8d797a7e7d52161ee1cb2e5a94da5387ffa3bfb6
paulyang1003 <paulyang1003@gmail.com>
2023-06-04 22:20:35 +0800

    新增 營業時間新增功能、營業時間修改功能、 clinicmember及businessDate兩表fetch合併

---
+++ src/main/resources/static/clinic/clinicAppointment/paul/frontstage/PetPet/js/businessDateManage.js
@@ -0,0 +1,48 @@
+function deleteClick(businessSn) {
+	if (!confirm('是否刪除?')) {
+		return;
+	}
+	fetch(`../../../../../business/${businessSn}`, {
+		method: `DELETE`,
+	})
+	location.reload();
+}
+
+function saveClick(businessSn) {
+	const vetSn = document.querySelector(`#vetSn${businessSn}`).textContent;
+	const weekDate = document.querySelector(`#weekDate${businessSn}`).textContent;
+	const morningBusiness = document.querySelector(`#morningBusiness${businessSn}`).textContent;
+	const morningAppointMax = document.querySelector(`#morningAppointMax${businessSn}`).textContent;
+	const afternoonBusiness = document.querySelector(`#afternoonBusiness${businessSn}`).textContent;
+	const afternoonAppointMax = document.querySelector(`#afternoonAppointMax${businessSn}`).textContent;
+	const nightBusiness = document.querySelector(`#nightBusiness${businessSn}`).textContent;
+	const nightAppointMax = document.querySelector(`#nightAppointMax${businessSn}`).textContent;
+
+	fetch(`../../../../../business/${businessSn}`, {
+		method: 'PUT',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			vetSn,
+			weekDate,
+			morningBusiness,
+			morningAppointMax,
+			afternoonBusiness,
+			afternoonAppointMax,
+			nightBusiness,
+			nightAppointMax
+		})
+	})
+		.then(resp => {
+			if (resp.ok) {
+				alert('修改成功!');
+				location.reload();
+			} else {
+				alert('修改失败');
+			}
+		})
+}
+
+
+
+
+
