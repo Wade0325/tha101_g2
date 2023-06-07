874cd812118ae4cc78c734c4a8614594db840d91
Andre <wza840808@gmail.com>
2023-06-07 10:12:54 +0800

    新增：forum

--- src/main/resources/static/forum/js/article_list_for_chat.js
+++ src/main/resources/static/forum/js/article_list_for_chat.js
@@ -1,28 +1,47 @@
 (() => {
 
-    const userId = document.querySelector('#userId');
-    var articleId = '';
+    // // 获取文章元素
+    // const articleContainer = document.querySelector('#articleContainer');
 
-    fetch('/forum/article_cat.html' + articleId)
-        .then(resp => resp.json())
-        .then(data => {
-            renderArticle(data);
-        })
-        .catch(error => {
-            console.error('发生错误', error);
-            alert("article_list_for_chat.js發生錯誤");
-        });
+    // //獲取文章資料並動態渲染
+    // fetch(`forum`, {
+    //     method: `GET`,
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
 
-    function renderArticle(article) {
-        const articleTitle = document.querySelector('.article-title');
-        const articleAuthor = document.querySelector('.article-author');
-        const articleTime = document.querySelector('.article-time');
-        const articleContent = document.querySelector('.article-content');
-
-        articleTitle.textContent = article.articleTitle;
-        articleAuthor.textContent = `作者：` + article.userId;
-        articleTime.textContent = `发表时间：` + article.articleCreateTime;
-        articleContent.textContent = article.articleContent;
-    }
+    //     })
+    // })
+    //     .then(resp => resp.json())
+    //     .then()
+   
 
 })()
+
+
+
+ // const userId = document.querySelector('#userId');
+    // var articleId = '';
+
+    // fetch('/forum/article_cat/' + articleId)    
+    //     .then(resp => resp.json())
+    //     .then(data => {
+    //         renderArticle(data);
+    //     })
+    //     .catch(error => {
+    //         console.error('发生错误', error);
+    //         alert("article_list_for_chat.js發生錯誤");
+    //     });
+
+    // function renderArticle(article) {
+    //     const articleTitle = document.querySelector('.article-title');
+    //     const articleAuthor = document.querySelector('.article-author');
+    //     const articleTime = document.querySelector('.article-time');
+    //     const articleContent = document.querySelector('.article-content');
+
+    //     articleTitle.textContent = article.articleTitle;
+    //     articleAuthor.textContent = `作者：` + article.userId;
+    //     articleTime.textContent = `发表时间：` + article.articleCreateTime;
+    //     articleContent.textContent = article.articleContent;
+    // }
