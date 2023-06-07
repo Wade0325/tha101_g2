d71c7e21fb2c4578226c11ac0a1b06b60b66df49
Paul <paulyang1003@gmail.com>
2023-06-07 13:35:27 +0800

    新增 clinicMember JPA、後臺公版

--- src/main/java/tw/idv/petpet/web/clinicMember/controller/ClinicMemberRegisterController.java
+++ src/main/java/tw/idv/petpet/web/clinicMember/controller/ClinicMemberRegisterController.java
@@ -1,5 +1,7 @@
 package tw.idv.petpet.web.clinicMember.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +19,7 @@ public class ClinicMemberRegisterController {
 	private ClinicMemberService service;
 	
 	@PostMapping
-	public ClinicMember register(@RequestBody ClinicMember clinicMember) {
+	public ClinicMember register(@RequestBody ClinicMember clinicMember, HttpSession session) {
 		if (clinicMember == null) {
 			clinicMember = new ClinicMember();
 			clinicMember.setMessage("無診所會員資訊");
@@ -26,4 +28,28 @@ public class ClinicMemberRegisterController {
 		}
 		return service.register(clinicMember);
 	}
+
+	@PostMapping("/login")
+	public ClinicMember login(@RequestBody ClinicMember clinicMember, HttpSession session) {
+		System.out.println("執行login方法");
+		ClinicMember clinicMemberSession = service.login(clinicMember);
+		if (clinicMemberSession.isSuccessful()) {
+			session.setAttribute("clinicEmail", clinicMemberSession);
+		}
+		System.out.println("login方法執行成功");
+		return clinicMemberSession;
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
