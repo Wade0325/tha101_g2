c404616ec93ee3a9eb3c0372936d1a743a1a0d59
Paul <paulyang1003@gmail.com>
2023-06-07 14:04:58 +0800

    解決報錯

--- src/main/java/tw/idv/petpet/web/clinicMember/Repository/ClinicMemberRepository.java
+++ src/main/java/tw/idv/petpet/web/clinicMember/Repository/ClinicMemberRepository.java
@@ -1,11 +1,15 @@
 package tw.idv.petpet.web.clinicMember.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import tw.idv.petpet.web.clinicMember.entity.ClinicMember;
 
 public interface ClinicMemberRepository extends JpaRepository<ClinicMember	, Integer>{
 
-	ClinicMember findByAccountAndPassword(String clinicEmail, String clinicPassword);
+	@Query("SELECT cm FROM ClinicMember cm WHERE cm.clinicEmail = :email AND cm.clinicPassword = :password")
+	ClinicMember findByEmailAndPassword(@Param("email") String clinicEmail, @Param("password") String clinicPassword);
+
 
 }
