88fd17fa7ead8b9b4b5b458b82ee6e324037f6c7
Wade <wl02014258@gmail.com>
2023-06-08 21:04:21 +0800

    新增: 查詢診所預約資料

--- src/main/resources/static/user/js/findByClinicName.js
+++ src/main/resources/static/user/js/findByClinicName.js
@@ -1,16 +1,10 @@
 (() => {
-
-
     console.log("aaa")
-    console.log("aaa")
-    fetch('../../../../clinicAppointment/all', {
-        method: 'POST',
+    fetch('../clinicAppointment', {
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: {
-
-        }
     })
         .then(resp => resp.json())
         .then(ClinicAppointment => {
@@ -18,24 +12,18 @@
                 console.log(appointment);
 
                 tbody.innerHTML += `
-				<tr>
-					<td>${appointment.reservationNumber}</td>
-					<td id="clinicName${appointment.reservationNumber}">${appointment.clinicName}</td>
-					<td id="vetName${appointment.reservationNumber}">${appointment.vetName}</td>
-					<td>${appointment.appointDate}</td>
-					<td id="appointTime${appointment.reservationNumber}">${appointment.appointTime}</td>
-					<td id="ownerMobile${appointment.reservationNumber}">${appointment.ownerMobile}</td>
-					<td id="petType${appointment.reservationNumber}">${appointment.petType}</td>
-					<td id="petName${appointment.reservationNumber}">${appointment.petName}</td>
-					<td id="serviceItem${appointment.reservationNumber}">${appointment.serviceItem}</td>
-					<td id="ownerName${appointment.reservationNumber}">${appointment.ownerName}</td>
-					<td id="petSituation${appointment.reservationNumber}">${appointment.petSituation}</td>
-					
-					<td>
-<!--						<button type="button" class="btn-edit" onclick="saveClick(${appointment.reservationNumber})">儲存</button> -->
-						<button type="button" class="btn-delete" onclick="deleteClick(${appointment.reservationNumber})">刪除</button>
-					</td>
-				</tr>`
+    				<tr>
+    				 	<td>${appointment.clinicName}</td>
+    					<td>${appointment.vetName}</td>
+    					<td>${appointment.appointDate}</td>
+    					<td>${appointment.appointTime}</td>
+    					<td>${appointment.ownerName}</td>
+                        <td>${appointment.ownerMobile}</td>
+    					<td>${appointment.petType}</td>
+    					<td>${appointment.petName}</td>
+    					<td>${appointment.serviceItem}</td>
+    					<td>${appointment.petSituation}</td>
+    				</tr>`
             }
         })
 })()
