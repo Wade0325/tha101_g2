8c5b5fbcb5f7178971d42e3da3e69cc6a31d5395
Paul <paulyang1003@gmail.com>
2023-06-08 17:47:03 +0800

    新增預約清單找主人方法

--- src/main/java/tw/idv/petpet/web/clinic/clinicAppointment/repository/ClinicAppointmentRepository.java
+++ src/main/java/tw/idv/petpet/web/clinic/clinicAppointment/repository/ClinicAppointmentRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tw.idv.petpet.web.clinic.clinicAppointment.entity.ClinicAppointment;
-import tw.idv.petpet.web.clinicMember.entity.ClinicMember;
 
 public interface ClinicAppointmentRepository extends JpaRepository<ClinicAppointment, Integer> {
 	
 	//自己寫的方法
 	List<ClinicAppointment> findByClinicName(String clinicName);
+	
+	List<ClinicAppointment> findByOwnerName(String ownerName);
 
 }
