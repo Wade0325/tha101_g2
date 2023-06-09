ca232eefdc19d75df6ba177618347e8f4b0a80c6
Wade <wl02014258@gmail.com>
2023-06-09 19:22:34 +0800

    新增: 商品查詢 刪除

--- src/main/java/tw/idv/petpet/web/shop/dao/ProductRepository.java
+++ src/main/java/tw/idv/petpet/web/shop/dao/ProductRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import tw.idv.petpet.web.shop.entity.Product;
@@ -12,4 +14,8 @@ public interface ProductRepository extends CrudRepository<Product, Integer>{
 
 	List<Product> findAll();
 	Page<Product> findAll(Pageable pageable);
+	
+	@Query(value = "DELETE FROM Product WHERE pro_name = ?1",nativeQuery = true)
+	@Modifying
+	Integer deleteByName(String proName);
 }
--- src/main/resources/static/shop/backstage/js/productManagement.js
+++ src/main/resources/static/shop/backstage/js/productManagement.js
@@ -4,18 +4,75 @@
     const pro_det = document.querySelector("#pro_det")
     const pro_price = document.querySelector("#pro_price")
     const pro_amount = document.querySelector("#pro_amount")
-
+    const tbody = document.querySelector("#tbody")
+    console.log("aaa")
     //後台取得商品
-    fetch('getProduct', {
+    fetch('../../getProduct', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
         },
     })
         .then(resp => resp.json())
-        .then(body => {
+        .then(data => {
+            console.log(data)
+            data.forEach(
+                item => {
+                    const { pro_name, cate_name, pro_det, pro_price, pro_amount } = item
+                    console.log(pro_name)
+                    const newRow = document.createElement('tr');
+                    newRow.innerHTML = `
+                        <td id="pro_name">${pro_name}</td>
+                        <td id="cate_name">${cate_name}</td>
+                        <td id="pro_det">${pro_det}</td>
+                        <td id="pro_price">${pro_price}</td>
+                        <td id="pro_amount">${pro_amount}</td>
+                        <td>
+                            <button class="btn btn-action btn-edit">編輯</button>
+                            <button class="btn btn-action btn-delete">刪除</button>
+                        </td>
+                    `;
+                    tbody.appendChild(newRow);
 
+
+                    // tbody.innerHTML +=
+                    //     `<tr>
+                    //     <td id="pro_name">${pro_name}</td>
+                    //     <td id="cate_name">${cate_name}</td>
+                    //     <td id="pro_det">${pro_det}</td>
+                    //     <td id="pro_price">${pro_price}</td>
+                    //     <td id="pro_amount">${pro_amount}</td>
+                    //     <td>
+                    //         <button class="btn btn-action btn-edit">編輯</button>
+                    //         <button class="btn btn-action btn-delete">刪除</button>
+                    //     </td>
+                    //     </tr>`
+                }
+            )
         })
 
+    tbody.addEventListener('click', function (event) {
+        if (event.target.classList.contains('btn-delete')) {
+            const row = event.target.closest('tr');
+            const pro_name = row.querySelector('#pro_name').textContent;
 
-})
+            // 在這裡執行刪除按鈕的處理邏輯
+            console.log(this);
+            const proName = document.querySelector("#pro_name").textContent
+            console.log(proName);
+
+            fetch(`../../delProduct/${proName}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'text/plain', // 將 Content-Type 設置為 'text/plain'
+                },
+                body: {
+
+                }
+            })
+
+            // 刪除
+            row.remove();
+        }
+    })
+})()
