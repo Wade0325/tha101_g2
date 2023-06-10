9202df4205fff6270d01146992fb498b94920651
Andre <wza840808@gmail.com>
2023-06-10 18:05:33 +0800

    1

--- src/main/java/tw/idv/petpet/web/forum/controller/ArticleCommentController.java
+++ src/main/java/tw/idv/petpet/web/forum/controller/ArticleCommentController.java
@@ -24,8 +24,13 @@ public class ArticleCommentController {
 	ArticleCommentService articleCommentService;
 
 	@PostMapping("/reply/{articleId}")
-	public void replyArticle(@PathVariable int articleId, @RequestBody ArticleComment articleComment) {
+	public void replyArticle(@PathVariable int articleId, @RequestBody ArticleComment articleComment, HttpSession session) {
 		// 调用 ArticleCommentService 处理回复文章的逻辑
+		User userSession = (User) session.getAttribute("userAccount");
+		if (userSession != null) {
+			Integer userId = userSession.getUserId();
+			System.out.println(userId);
+		}
 		articleCommentService.replyArticle(articleId, articleComment);
 	}
 
@@ -36,15 +41,12 @@ public class ArticleCommentController {
 //		System.out.println("查看是否獲取Id" + articleId);
 //		return findArticleId;
 //	}
-	
+
 	@GetMapping("article_cat/{articleId}")
-	public List<ArticleComment> findByArticleId(@PathVariable Integer articleId, HttpSession session) {
+	public List<ArticleComment> findByArticleId(@PathVariable Integer articleId) {
+
 		
-		User userSession = (User) session.getAttribute("userAccount");
-		userSession.getUserAccount();
-		userSession.getUserId();
-		
-		
+
 		System.out.println("ArticleCommentController 開始執行");
 		List<ArticleComment> findArticleIdAll = articleCommentService.findByArticleId(articleId);
 		System.out.println("查看是否獲取Id" + articleId);
